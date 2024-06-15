ca001ad053b5552be0cda7e7679ac2595e444193
IntriguedSayan <sayan.mid590@gmail.com>
2024-06-15 21:49:06 +0530

    feat: new migrations added

---
+++ prisma/migrations/20240615161632_assignment/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "Contact_email_key";
+
+-- DropIndex
+DROP INDEX "Contact_phoneNumber_key";
+
+-- DropIndex
+DROP INDEX "email_idx";
+
+-- DropIndex
+DROP INDEX "linkedId_idx";
+
+-- DropIndex
+DROP INDEX "phone_idx";
